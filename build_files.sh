4d5b047b17b7ddcd0b7a6ad2d6569635a569cfb3
Dispenser254 <stevensonkibs55@gmail.com>
2024-03-29 00:30:00 +0300

    Updated commit

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
+
+# Create virtual environment if it doesn't exist
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+fi
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Install requirements
 pip install -r requirements.txt
 
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
-
-python3.9 manage.py collectstatic --noinput --clear
+# Run Django management commands
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput --clear
